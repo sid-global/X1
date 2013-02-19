521932c5afc50a8ce7f3b83e84ce09831689cb62
SID CA <SID CA>
2013-02-18 22:29:37 -0430

    c8
    c8

---
+++ Resources/argconfig.js
@@ -0,0 +1,16 @@
+AC.Config.set({
+    apiAdapter : "Web",
+    needFlashVersion : "9.0.0",
+    clientApi : "http://sid.applicationcraft.com/",
+    serverUrl : "http://sid.applicationcraft.com/service/",
+    serverUploadUrl : "http://sid.applicationcraft.com/service/",
+    uploadServerPath: "http://sid.applicationcraft.com/service/uploads/",
+    uploadServerDirectPath: "http://sid.applicationcraft.com/service/uploads/direct.php",
+    serverEventsUrl: 'http://notify.applicationcraft.com/events',
+    formatApi : "json",
+    serverThemes: "themes/",
+    localAppsPath: 'Apps/',
+    flashAdapterUrl : "wiziCore/api/server/adapter/WiziSendRequest.swf",
+    isDebugRequest : true,
+    googleApiKey: {"rows":[{"id":1,"data":["http://applicationcraft.com/","ABQIAAAACfMroKb5WzfdVTHbQi2OfRRGQFuqEDC-bwWLPZ60Ces-G56XXhTiOZYFBXlh0_EId2nXJCwq7iTNIg"],"ind":1}]}
+});
